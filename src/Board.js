4abbb72d70f8451153dd389734357295f92e9d27
axuy <thealbertxu@gmail.com>
2017-05-17 02:33:32 -0400

    added piece icons

--- src/Board.js
+++ src/Board.js
@@ -2,71 +2,72 @@ import React from 'react';
 import Square from './Square.js'
  
 class Board extends React.Component {
-  renderSquare(cellKey, piece, owner) {
+  renderSquare(cellId, piece, owner) {
     return (
       <Square
-        key={cellKey}
+        key={cellId}
+        cellId={cellId}
         piece={piece}
         owner={owner}
-        onClick={() => this.handleClick(cellKey)}
+        onClick={() => this.handleClick(cellId)}
       />
     );
   }
 
-  handleClick(cellKey) {
+  handleClick(cellId) {
 
   }
 
   render() {
     var rows = [];
     var columns = [];
-    var cellKey = 0;
+    var cellId = 0;
     for (var i = 0; i < 8; i++) {
       for (var j = 0; j < 8; j++) {
         if(i == 1) {
-          columns.push(this.renderSquare(cellKey, 'P', "black"))
+          columns.push(this.renderSquare(cellId, 'P', "black"))
         }
         else if(i == 6) {
-          columns.push(this.renderSquare(cellKey, 'P', "white"))
+          columns.push(this.renderSquare(cellId, 'P', "white"))
         }
         else if(i == 0) {
           if(j == 0 || j == 7) {
-            columns.push(this.renderSquare(cellKey, 'R', "black"))
+            columns.push(this.renderSquare(cellId, 'R', "black"))
           }
           if(j == 1 || j == 6) {
-            columns.push(this.renderSquare(cellKey, 'N', "black"))
+            columns.push(this.renderSquare(cellId, 'N', "black"))
           }
           if(j == 2 || j == 5) {
-            columns.push(this.renderSquare(cellKey, 'B', "black"))
+            columns.push(this.renderSquare(cellId, 'B', "black"))
           }
           if(j == 3) {
-            columns.push(this.renderSquare(cellKey, 'Q', "black"))
+            columns.push(this.renderSquare(cellId, 'Q', "black"))
           }
           if(j == 4) {
-            columns.push(this.renderSquare(cellKey, 'K', "black"))
+            columns.push(this.renderSquare(cellId, 'K', "black"))
           }
         }
         else if(i == 7) {
           if(j == 0 || j == 7) {
-            columns.push(this.renderSquare(cellKey, 'R', "white"))
+            columns.push(this.renderSquare(cellId, 'R', "white"))
           }
           if(j == 1 || j == 6) {
-            columns.push(this.renderSquare(cellKey, 'N', "white"))
+            columns.push(this.renderSquare(cellId, 'N', "white"))
           }
           if(j == 2 || j == 5) {
-            columns.push(this.renderSquare(cellKey, 'B', "white"))
+            columns.push(this.renderSquare(cellId, 'B', "white"))
           }
           if(j == 3) {
-            columns.push(this.renderSquare(cellKey, 'Q', "white"))
+            columns.push(this.renderSquare(cellId, 'Q', "white"))
           }
           if(j == 4) {
-            columns.push(this.renderSquare(cellKey, 'K', "white"))
+            columns.push(this.renderSquare(cellId, 'K', "white"))
           }
         }
         else {
-          columns.push(this.renderSquare(cellKey, null))
+          columns.push(this.renderSquare(cellId, null))
         }
-        cellKey++
+        cellId++
       }
       rows.push(<div className="board-row" key={"row" + i}>{columns}</div>)
       columns = [];
--- src/Square.js
+++ src/Square.js
@@ -2,12 +2,60 @@ import React from 'react'
 
 class Square extends React.Component {
   render() {
-
+    var squareColor = "";
+    if(this.props.cellId % 2 === 0 && this.props.cellId % 16 < 8 ||
+       this.props.cellId % 2 === 1 && this.props.cellId % 16 > 7) {
+      squareColor = "white-square";
+    }
+    else {
+      squareColor = "black-square";
+    }
+    if(this.props.piece === null) {
+      return (
+        <div className={squareColor} onClick={this.props.onClick}/>
+      );
+    }
+    var imgSrc = "";
+    switch (this.props.owner + this.props.piece) {
+      case "whiteP":
+        imgSrc = './images/white_pawn.png';
+        break;
+      case "whiteR":
+        imgSrc = './images/white_rook.png';
+        break;
+      case "whiteN":
+        imgSrc = './images/white_knight.png';
+        break;
+      case "whiteB":
+        imgSrc = './images/white_bishop.png';
+        break;
+      case "whiteQ":
+        imgSrc = './images/white_queen.png';
+        break;
+      case "whiteK":
+        imgSrc = './images/white_king.png';
+        break;
+      case "blackP":
+        imgSrc = './images/black_pawn.png';
+        break;
+      case "blackR":
+        imgSrc = './images/black_rook.png';
+        break;
+      case "blackN":
+        imgSrc = './images/black_knight.png';
+        break;
+      case "blackB":
+        imgSrc = './images/black_bishop.png';
+        break;
+      case "blackQ":
+        imgSrc = './images/black_queen.png';
+        break;
+      case "blackK":
+        imgSrc = './images/black_king.png';
+    }
     return (
-      <div className="square" onClick={props.onClick}>
-        {if(props.piece='P') {
-
-        }}
+      <div className={squareColor} onClick={this.props.onClick}>
+        <img src={imgSrc}/>
       </div>
     );
   }
